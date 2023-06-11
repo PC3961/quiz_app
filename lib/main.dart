740f760e442c25e1a7eb6907737985e0d2c9b79e
PC3961 <cpranavss65@gmail.com>
2023-06-12 00:15:24 +0530

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/gradient_container.dart';
+import 'package:quiz_app/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(
+    const MaterialApp(
     home: Scaffold(
-      body: GradientContainer(
+      body: StartScreen(
         Colors.deepOrange,
         Colors.orangeAccent,
       ),
---
+++ lib/start_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return const Text('Start Screen');
+  }
+}
