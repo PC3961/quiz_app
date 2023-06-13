e67c1b0310c0887f37fe0af44f926890392176c9
PC3961 <cpranavss65@gmail.com>
2023-06-13 19:07:24 +0530

    hi

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -11,6 +11,14 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
@@ -22,7 +30,7 @@ class _QuizState extends State<Quiz> {
               Colors.deepOrangeAccent,
             ], begin: Alignment.topLeft, end: Alignment.bottomRight),
           ),
-          child: const StartScreen(),
+          child: activeScreen,
         ),
       ),
     );
