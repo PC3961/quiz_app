05ae5d12c6bba45a916600ca0da2492fc5e0ae66
PC3961 <cpranavss65@gmail.com>
2023-06-15 23:14:14 +0530

    Alignment,Margin and Padding

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -17,18 +17,22 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     final currentQuestion = questions[0];
     return SizedBox(
       width: double.infinity, // Maximum width possible
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(
-          currentQuestion.text,
-          style: const TextStyle(
-            color: Colors.white,
+      child: Container(
+        margin: const EdgeInsets.all(20),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
+              color: Colors.white,
+            ),
           ),
-        ),
-        const SizedBox(height: 30),
-        ...currentQuestion.answers.map((answer) {
-          return AnswerButton(answerText: answer, onTap: () {});
-        }),
-      ]),
+          const SizedBox(height: 30),
+          ...currentQuestion.answers.map((answer) {
+            return AnswerButton(answerText: answer, onTap: () {});
+          }),
+        ]),
+      ),
     );
   }
 }
