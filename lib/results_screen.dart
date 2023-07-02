1a242abea3061427e1d20fffb1972d769ca25453
PC3961 <cpranavss65@gmail.com>
2023-07-02 23:33:39 +0530

    maps and for loops

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,10 +1,25 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 /* The ResultsScreen class is a StatelessWidget that displays the user's chosen answers, the number of
  correct answers, and a button to restart the quiz.*/
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
   final List<String> chosenAnswers;
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_index': i,
+        'questions': questions[i].text,
+        'correct_answers': questions[i].answers[0],
+        'user_answer': chosenAnswers[i]
+      });
+    }
+    return summary;
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -12,13 +27,20 @@ class ResultsScreen extends StatelessWidget {
       child: Container(
         margin: const EdgeInsets.all(40),
         child: Column(
-          children:  [const Text('You answered the questions X out of Y correctly !'),
-          const SizedBox(height:30 ,),
-          const Text('List of answers and questions...'),
-          const SizedBox(height: 30,),
-          TextButton(onPressed: (){}, child:const Text ('Restart Quiz'),)
+          children: [
+            const Text('You answered the questions X out of Y correctly !'),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text('List of answers and questions...'),
+            const SizedBox(
+              height: 30,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text('Restart Quiz'),
+            )
           ],
-          
         ),
       ),
     );
